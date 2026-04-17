81cc856bb4c538e0ddfaedd03c69db5caadb2a6a
hak1ares <dionysuse0@gmail.com>
2026-04-17 04:19:52 +0300

    worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,26 @@
+export default {
+  async fetch(request) {
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+          "Access-Control-Allow-Headers": "*",
+        },
+      });
+    }
 
+    const url = new URL(request.url);
+    const nimUrl = "https://integrate.api.nvidia.com" + url.pathname;
+
+    const response = await fetch(nimUrl, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    });
+
+    const newResponse = new Response(response.body, response);
+    newResponse.headers.set("Access-Control-Allow-Origin", "*");
+    return newResponse;
+  },
+};
